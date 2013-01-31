d7963187e72274133f85ab9d27a1b8bfeb74368d
Longkai Cao <lkcao@thoughtworks.com>
2013-01-31 10:23:13 +0800

    add movieType enum instances and constuctor

--- video-rental/src/com/twu/refactor/MovieType.java
+++ video-rental/src/com/twu/refactor/MovieType.java
@@ -1,7 +1,21 @@
 package com.twu.refactor;
 
 public enum MovieType {
+    regular(0, 2, 2, 1.5),
+    new_release(1, 0, 0, 3),
+    children(2, 3, 1.5, 1.5);
 
+    private final int typeCode;
+    private final int baseDays;
+    private final double basePrice;
+    private final double unitPrice;
+
+    private MovieType(int typeCode, int baseDays, double basePrice, double unitPrice) {
+        this.typeCode = typeCode;
+        this.baseDays = baseDays;
+        this.basePrice = basePrice;
+        this.unitPrice = unitPrice;
+    }
 
     static double getBasePrice(double[] basePrice, int priceCode) {
         return basePrice[priceCode];
